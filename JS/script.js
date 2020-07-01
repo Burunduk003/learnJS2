53966bb6adfb05e3cfc42ac715f3ce1de284ae64
Anton <antoniok003@gmail.com>
2020-07-01 14:06:29 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+//console.log(1)
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// alert("Hello");
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = prompt("вам есть 18?", "Да");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('ваша фамилия?', '');
+// answers[2] = prompt('сколько вам лет?', '');
+
+// document.write(answers);
+
+// console.log(typeof(answers));
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+// = присваивание
+// == равенство
+// === строгое равенство (с учетом типа данных)
+
+const isChacked = true,
+      isClose = true;
+console.log(isChacked && isClose);
