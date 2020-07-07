814e8e4a95e80dc2cc7efa61c80c68884722c01e
Anton <antoniok003@gmail.com>
2020-07-07 17:24:56 +0300

    воторое задание

--- JS/script.js
+++ JS/script.js
@@ -10,12 +10,58 @@ const PersonalMuvieDB = {
     private: false
 };
 
-const q = prompt("Один из последних просмотренных фильмов?", ''),
-    q2 = prompt("На сколько оцените его?", ''),
-    q1 = prompt("Один из последних просмотренных фильмов?", ''),
-    q4 = prompt("На сколько оцените его?", '');
 
-PersonalMuvieDB.movies[q] = q2;
-PersonalMuvieDB.movies[q1] = q4;
+for (let i = 0; i < 2; i++) {
+    const q1 = prompt("Один из последних просмотренных фильмов?", ''),
+          q2 = prompt("На сколько оцените его?", '');
 
-console.log(PersonalMuvieDB);
+    if (q1 != null && q2 != null && q1 != '' && q2 != '' && q1.length < 50) {
+        PersonalMuvieDB.movies[q1] = q2;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+// let i = 0;
+// while (i < 2) {
+//     const q1 = prompt("Один из последних просмотренных фильмов?", ''),
+//     q2 = prompt("На сколько оцените его?", '');
+//     i++;
+//     if (q1 != null && q2 != null && q1 != '' && q2 != '' && q1.length < 50) {
+//         PersonalMuvieDB.movies[q1] = q2;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }    
+
+// let i = 0;
+// do {
+//     const q1 = prompt("Один из последних просмотренных фильмов?", ''),
+//         q2 = prompt("На сколько оцените его?", '');
+//     i++;
+//     if (q1 != null && q2 != null && q1 != '' && q2 != '' && q1.length < 50) {
+//         PersonalMuvieDB.movies[q1] = q2;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }    
+// } while (i < 2);
+
+
+
+if (PersonalMuvieDB.count <= 10) {
+        console.log("просмотрено довольно мало фильмов");
+    } else if (PersonalMuvieDB.count > 10 && PersonalMuvieDB.count <= 30) {
+        console.log("вы классный зритель");
+    } else if (PersonalMuvieDB.count > 30) {
+        console.log("вы киноман");    
+    } else {
+        console.log("Произошла ошибка");
+    }
+
+    console.log(PersonalMuvieDB);
--- learn.js
+++ learn.js
@@ -56,3 +56,59 @@
 // const isChacked = false,
 //       isClose = true;
 // console.log(isChacked || isClose);
+
+// if (4 == 9) {
+//     console.log("ok");
+// } else {
+//     console.log("error");
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log("error");
+// } else if (num > 100) {
+//     console.log("много");
+// } else {
+//     console.log("ок!");
+// }
+
+// (num === 50) ? console.log('ok!') : console.log("error");
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('error');
+//         break;
+//     case 100:
+//         console.log('error');
+//         break;    
+//     case 51:
+//         console.log('Ok!')    
+//         break;
+//     default:
+//         console.log('не в этот раз!');
+//         break;    
+// }
+
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+    console.log(i);
+}
